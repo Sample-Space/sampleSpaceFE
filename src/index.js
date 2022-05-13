fdb3becb2479a0f56738fd30d4fa0e4dd2185fa7
Jordan Skomal <jskomal@gmail.com>
2022-05-13 15:24:44 -0600

    Implement Play Route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
+import Play from './components/Play/Play'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -8,7 +9,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />}></Route>
-      {/* play route */}
+      <Route exact path='/play' element={<Play />}></Route>
     </Routes>
   </BrowserRouter>
 )
