e9b537d6b1aed2c81fcfab193f880f527ba06dc3
Jordan Skomal <jskomal@gmail.com>
2022-05-13 15:10:53 -0600

    Add Logo Svg

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,30 @@
 import './App.css'
+import logo from '../../assets/images/sample-space-logo.svg'
+import { Link } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>HELLO</h1>
+    <div className='app'>
+      <header>
+        <img src={logo} alt='Sample Space logo' />
+        <h2>an eductaional drum machine featuring samples from across the universe</h2>
+      </header>
+      <main className='main-view'>
+        <h3 className='main-item'>Learn How to Play:</h3>
+        <iframe
+          className='main-item'
+          width='560'
+          height='315'
+          src='https://www.youtube.com/embed/dokLwszdUgY'
+          title='YouTube video player'
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        ></iframe>
+        <Link to='/play'>
+          <button>Let's Jam</button>
+        </Link>
+      </main>
     </div>
   )
 }
