aaf843fea0cac2c3ac29a5157ae1d36c68a9b130
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-16 13:04:58 -0600

    Link button to audio components

--- src/components/DrumPad/DrumPad.js
+++ src/components/DrumPad/DrumPad.js
@@ -12,7 +12,20 @@ const DrumPad = ({ padId }) => {
       onMouseUp={()=> setNotes(null)} >
       <img className="pad-image" src="https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop" />
     </button>
-
+    <Song>
+      <Track>
+        <Instrument
+          type="sampler"
+          notes={notes}
+          samples={{
+            'C3': kick
+          }}
+          onLoad={(buffers) => {
+            console.log('Were all loaded')
+          }}
+           />
+      </Track>
+    </Song>
     </>
   )
 }
