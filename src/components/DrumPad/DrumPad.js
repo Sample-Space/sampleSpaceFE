b38e6d24366a8d5c9cf31f6ef51f32322ce2b6d2
Eric Matlock <ermatlock@gmail.com>
2022-05-16 07:51:10 -0700

    Create DrumPad component

---
+++ src/components/DrumPad/DrumPad.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./DrumPad.css"
+
+const DrumPad = ({ padId }) => {
+  return (
+    <button className="drum-pad" id={padId} >
+      <img src="" />
+    </button>
+  )
+}
+
+export default DrumPad
