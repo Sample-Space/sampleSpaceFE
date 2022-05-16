7ba52d5b7860a7c0e65859229318a98626b9b9e8
Eric Matlock <ermatlock@gmail.com>
2022-05-16 08:45:43 -0700

    Add single pad for testing

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,14 +1,14 @@
 import { Song, Track, Instrument, Effect } from "reactronica";
-import logo from '../../assets/images/sample-space-logo.svg'
+import logo from "../../assets/images/sample-space-logo.svg";
 import DrumPad from "../DrumPad/DrumPad";
 
 const Play = () => {
-  
   return (
     <>
       <header>
         <img src={logo} alt="Sample Space logo" />
       </header>
+      <DrumPad />
       <div className="play-container"></div>
     </>
   );
