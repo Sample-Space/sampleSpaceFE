2c5f20c7c99a78cf1f4aeefec2cde0dc06a68573
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-17 15:54:03 -0600

    Refactor test for readability

--- cypress/integration/welcome_spec.js
+++ cypress/integration/welcome_spec.js
@@ -1,8 +1,11 @@
 describe('Sample Space', () => {
-  it('Should load up the main page', () => {
+  beforeEach(() => {
     cy.visit('localhost:3000')
-      .get('header img')
+  })
+  it('Should have a header with an image and description of the app', () => {
+    cy.get('header img')
+        .should('have.attr', 'src', '/static/media/sample-space-logo.0b8cea83f06fa8ea8674e40bbf7391fb.svg')
       .get('header h2')
-      .get('main button')
+        .contains('an educational drum machine featuring samples from across the universe')
   })
 })
