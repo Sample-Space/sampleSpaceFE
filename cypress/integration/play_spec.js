713183149c9b213911707880c983a12bbaaf8e4e
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-17 16:58:48 -0600

    Test that all buttons are clickable with an image

--- cypress/integration/play_spec.js
+++ cypress/integration/play_spec.js
@@ -7,4 +7,18 @@ describe('/play endpoint', () => {
    cy.get('header img')
     .should('have.attr', 'src', '/static/media/sample-space-logo.0b8cea83f06fa8ea8674e40bbf7391fb.svg')
  })
+
+ it('Should have 8 clickable buttons', () => {
+   let buttons = cy.get('.pad-container')
+
+    buttons.children()
+      .should('have.length', 8)
+
+    buttons.each(button => {
+      cy.wrap(button).children()
+        .first()
+        .should('have.attr', 'src', 'https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop')
+        .parent().click()
+      })
+ })
 })
