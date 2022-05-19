0126e575586c2f9852f3cec149c834a5ac6b4ae2
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-19 10:26:46 -0600

    Pass notes as props

--- src/components/DrumPad/DrumPad.js
+++ src/components/DrumPad/DrumPad.js
@@ -10,48 +10,56 @@ const DrumPad = () => {
     <div className='pad-container'>
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'E3'}
         id={'sample-1'}
         imgSrc={sampleKit.kit1[0].img}
       />
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'F3'}
         id={'sample-2'}
         imgSrc={sampleKit.kit1[1].img}
       />
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'F#3'}
         id={'sample-3'}
         imgSrc={sampleKit.kit1[2].img}
       />
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'G3'}
         id={'groove'}
         imgSrc={sampleKit.kit1[3].img}
       />
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'C3'}
         id={'kick'}
         imgSrc={sampleKit.kit1[4].img}
       />
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'C#3'}
         id={'snare'}
         imgSrc={sampleKit.kit1[5].img}
       />
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'D3'}
         id={'hh1'}
         imgSrc={sampleKit.kit1[6].img}
       />
       <DrumPadButton
         setNotes={setNotes}
+        notes={notes}
         note={'D#3'}
         id={'hh2'}
         imgSrc={sampleKit.kit1[7].img}
