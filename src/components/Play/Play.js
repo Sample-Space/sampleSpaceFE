a44688aba3470830b27452a98c66f9458aa5c35f
Jordan Skomal <jskomal@gmail.com>
2022-05-19 14:48:36 -0600

    Add Links to homepage

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Play.css'
 import logo from '../../assets/images/sample-space-logo.svg'
 import DrumPad from '../DrumPad/DrumPad'
 import InfoBox from '../InfoBox/InfoBox'
+import { Link } from 'react-router-dom'
 
 const Play = () => {
   const [currentSample, setCurrentSample] = useState(null)
+
   return (
     <div className='main-view'>
       <header>
-        <img src={logo} alt='Sample Space logo' />
+        <Link to='/'>
+          <img src={logo} alt='Sample Space logo' />
+        </Link>
       </header>
       <main className='main-container'>
-        <DrumPad />
+        <DrumPad setCurrentSample={setCurrentSample} />
         <InfoBox currentSample={currentSample} />
       </main>
     </div>
