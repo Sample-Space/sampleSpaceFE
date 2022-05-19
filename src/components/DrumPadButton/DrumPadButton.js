4ce2e7144d4c5a212672eabc64305c36557cfb6e
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-19 10:27:20 -0600

    Refactor mouse events to click

--- src/components/DrumPadButton/DrumPadButton.js
+++ src/components/DrumPadButton/DrumPadButton.js
@@ -1,13 +1,22 @@
 import './DrumPadButton.css'
 
-const DrumPadButton = ({ setNotes, note, id, imgSrc }) => {
+const DrumPadButton = ({ setNotes, notes, note, id, imgSrc }) => {
+
+  const play = async () => {
+    await (notes === null ? setNotes([{name: note, duration: 3}]) : '')
+    reset();
+  }
+
+  const reset = async () => {
+    await setNotes(null)
+  }
+
   return (
     <>
       <button
         className='drum-pad'
         id={id}
-        onMouseDown={() => setNotes([{ name: note }])}
-        onMouseUp={() => setNotes(null)}
+        onClick={() => play()}
       >
         <img className='pad-image' src={imgSrc} />
       </button>
