81732baa5d3fa9558de142332e59619498811e98
Jordan Skomal <jskomal@gmail.com>
2022-05-19 10:13:16 -0600

    Add main view wrapper and begin to add curent sample state

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,19 +1,21 @@
+import { useState } from 'react'
 import './Play.css'
 import logo from '../../assets/images/sample-space-logo.svg'
 import DrumPad from '../DrumPad/DrumPad'
 import InfoBox from '../InfoBox/InfoBox'
 
 const Play = () => {
+  const [currentSample, setCurrentSample] = useState(null)
   return (
-    <>
+    <div className='main-view'>
       <header>
         <img src={logo} alt='Sample Space logo' />
       </header>
       <main className='main-container'>
         <DrumPad />
-        <InfoBox />
+        <InfoBox currentSample={currentSample} />
       </main>
-    </>
+    </div>
   )
 }
 
