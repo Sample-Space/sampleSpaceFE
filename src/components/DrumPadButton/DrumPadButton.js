247585af5b1b880f410b04bd8b283efb918649b6
Jordan Skomal <jskomal@gmail.com>
2022-05-20 16:36:48 -0600

    Change MouseDown/Up to OnClick

--- src/components/DrumPadButton/DrumPadButton.js
+++ src/components/DrumPadButton/DrumPadButton.js
@@ -1,12 +1,18 @@
 import './DrumPadButton.css'
 
-const DrumPadButton = ({ setNotes, note, id, imgSrc, onClick }) => {
+const DrumPadButton = ({ setNotes, note, id, imgSrc, kickRef }) => {
+  const onClick = async () => {
+    await setNotes([{ name: note, duration: 4 }])
+    await setNotes(null)
+  }
+
   return (
     <button
+      ref={kickRef}
       className='drum-pad'
       id={id}
-      onMouseDown={() => setNotes([{ name: note, duration: 4 }])}
-      onMouseUp={() => setNotes(null)}
+      // onMouseDown={() => setNotes([{ name: note, duration: 4 }])}
+      // onMouseUp={() => setNotes(null)}
       onClick={onClick}
     >
       <img className='pad-image' src={imgSrc} />
