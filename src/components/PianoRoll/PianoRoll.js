37c86b6d60dfaae40c50645012fcf2fbccd57ced
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-23 15:00:33 -0600

    Initialize react hooks

--- src/components/PianoRoll/PianoRoll.js
+++ src/components/PianoRoll/PianoRoll.js
@@ -3,7 +3,26 @@ import { Song, Track, Instrument } from 'reactronica';
 
 
 const PianoRoll = () => {
-  
+  const [isPlaying, setIsPlaying] - useState(false);
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  const [steps, setSteps] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+  ])
     return (
         <h1>PIANO ROLL</h1>
     )
