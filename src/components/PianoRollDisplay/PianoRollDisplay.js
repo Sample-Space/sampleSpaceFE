809dc39d7e94ce47d2fbc0330f8f9aa4131827e0
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-23 17:47:53 -0600

    Create PianoRoll template

--- src/components/PianoRollDisplay/PianoRollDisplay.js
+++ src/components/PianoRollDisplay/PianoRollDisplay.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import './PianoRollDisplay.css';
+import PianoRow from '../PianoRow/PianoRow.js'
 
-const PianoRollDisplay = () => {
+const PianoRollDisplay = ({currentStepIndex, steps }) => {
     return (
-        <section>
-          <
+        <section >
+          <PianoRow />
+          <PianoRow
+            name={'Kick'}
+            note={'C3'}
+            currentStepIndex={currentStepIndex}
+             />
+          <PianoRow
+            name={'Snare'}
+            note={'C#3'}
+            currentStepIndex={currentStepIndex}
+            />
+          <PianoRow
+            name={'High Hat 1'}
+            note={'D3'}
+            currentStepIndex={currentStepIndex}
+            />
+          <PianoRow
+            name={'High Hat 2'}
+            note={'D#3'}
+            currentStepIndex={currentStepIndex}
+            />
+          <PianoRow
+            name={'Melody'}
+            note={'E3'}
+            currentStepIndex={currentStepIndex}
+            />
+          <PianoRow
+            name={'One Shot 1'}
+            note={'F3'}
+            currentStepIndex={currentStepIndex}
+            />
+          <PianoRow
+            name={'One Shot 2'}
+            note={'F#3'}
+            currentStepIndex={currentStepIndex}
+            />
+          <PianoRow
+            name={'Texture'}
+            note={'G3'}
+            currentStepIndex={currentStepIndex}
+            />
         </section>
     )
 }
