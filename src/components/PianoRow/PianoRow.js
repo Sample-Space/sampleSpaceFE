99266b17db3cdaa8735a05d47e9a9c3b36a986f9
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-23 18:56:27 -0600

    Create row with buttons

--- src/components/PianoRow/PianoRow.js
+++ src/components/PianoRow/PianoRow.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './PianoRow.css'
 
-const PianoRow = () => {
+const PianoRow = ({ name, note, steps, currentStepIndex, setSteps }) => {
+  
+  let row = (
+    <div className='row'>
+    {name ? <p>{name}</p> : <p> </p>}
+    {name ? steps.map((step, index) => steps[index] && steps[index].includes(note) ? <button onClick={() => toggleNote(note, index)} key={index}>{index}</button> : <button onClick={} >Pog</button>) : steps.map((step, index) => <p key={index}>{index + 1}</p>)}
+    </div>
+  )
+
+
     return (
-        <div>
-        </div>
+      <>
+      {row}
+      </>
     )
 }
 
