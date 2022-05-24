0c9e6ef6e433863bc82d9f05ef80939d9472a410
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-23 18:54:19 -0600

    Pass steps and SetSteps to PianoRow

--- src/components/PianoRollDisplay/PianoRollDisplay.js
+++ src/components/PianoRollDisplay/PianoRollDisplay.js
@@ -2,48 +2,67 @@ import React from 'react';
 import './PianoRollDisplay.css';
 import PianoRow from '../PianoRow/PianoRow.js'
 
-const PianoRollDisplay = ({currentStepIndex, steps }) => {
+const PianoRollDisplay = ({currentStepIndex, steps, setSteps }) => {
     return (
-        <section >
-          <PianoRow />
+        <section className="roll-display" >
+          <PianoRow
+            currentStepIndex={currentStepIndex}
+            steps={steps}
+            />
           <PianoRow
             name={'Kick'}
             note={'C3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
              />
           <PianoRow
             name={'Snare'}
             note={'C#3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
             />
           <PianoRow
             name={'High Hat 1'}
             note={'D3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
             />
           <PianoRow
             name={'High Hat 2'}
             note={'D#3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
             />
           <PianoRow
             name={'Melody'}
             note={'E3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
             />
           <PianoRow
             name={'One Shot 1'}
             note={'F3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
             />
           <PianoRow
             name={'One Shot 2'}
             note={'F#3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
             />
           <PianoRow
             name={'Texture'}
             note={'G3'}
+            steps={steps}
+            setSteps={setSteps}
             currentStepIndex={currentStepIndex}
             />
         </section>
