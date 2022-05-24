b1db56162c0c7608d8d6267e0e9e439689421ad7
Jordan Skomal <jskomal@gmail.com>
2022-05-24 14:39:07 -0600

    Change names into 4 char

--- src/components/PianoRollDisplay/PianoRollDisplay.js
+++ src/components/PianoRollDisplay/PianoRollDisplay.js
@@ -1,72 +1,69 @@
-import React from 'react';
-import './PianoRollDisplay.css';
+import React from 'react'
+import './PianoRollDisplay.css'
 import PianoRow from '../PianoRow/PianoRow.js'
 
-const PianoRollDisplay = ({currentStepIndex, steps, setSteps }) => {
-    return (
-        <section className="roll-display" >
-          <PianoRow
-            currentStepIndex={currentStepIndex}
-            steps={steps}
-            />
-          <PianoRow
-            name={'Kick'}
-            note={'C3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-             />
-          <PianoRow
-            name={'Snare'}
-            note={'C#3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-            />
-          <PianoRow
-            name={'High Hat 1'}
-            note={'D3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-            />
-          <PianoRow
-            name={'High Hat 2'}
-            note={'D#3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-            />
-          <PianoRow
-            name={'Melody'}
-            note={'E3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-            />
-          <PianoRow
-            name={'One Shot 1'}
-            note={'F3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-            />
-          <PianoRow
-            name={'One Shot 2'}
-            note={'F#3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-            />
-          <PianoRow
-            name={'Texture'}
-            note={'G3'}
-            steps={steps}
-            setSteps={setSteps}
-            currentStepIndex={currentStepIndex}
-            />
-        </section>
-    )
+const PianoRollDisplay = ({ currentStepIndex, steps, setSteps }) => {
+  return (
+    <section className='roll-display'>
+      <PianoRow currentStepIndex={currentStepIndex} steps={steps} />
+      <PianoRow
+        name={'KICK'}
+        note={'C3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+      <PianoRow
+        name={'SNRE'}
+        note={'C#3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+      <PianoRow
+        name={'HHCL'}
+        note={'D3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+      <PianoRow
+        name={'HHOP'}
+        note={'D#3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+      <PianoRow
+        name={'MELO'}
+        note={'E3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+      <PianoRow
+        name={'OS-1'}
+        note={'F3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+      <PianoRow
+        name={'OS-2'}
+        note={'F#3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+      <PianoRow
+        name={'OS-3'}
+        note={'G3'}
+        steps={steps}
+        setSteps={setSteps}
+        currentStepIndex={currentStepIndex}
+      />
+    </section>
+  )
 }
 
 export default PianoRollDisplay
