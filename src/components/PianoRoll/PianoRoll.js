9f4ecbcad4d4274434ce9936f45cddf971bc2505
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-23 18:53:54 -0600

    Change prop name to setSteps

--- src/components/PianoRoll/PianoRoll.js
+++ src/components/PianoRoll/PianoRoll.js
@@ -35,7 +35,7 @@ const PianoRoll = ({ kit }) => {
         <PianoRollDisplay
         currentStepIndex={currentStepIndex}
         steps={steps}
-        onclick={(steps) => setSteps(steps)}
+        setSteps={setSteps}
         />
 
 
