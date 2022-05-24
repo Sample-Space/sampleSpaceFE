3a718cc0135648d080eb599562e12d5cc7732d0c
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-23 20:30:25 -0600

    Create rows of buttons or numbers with functionality

--- src/components/PianoRow/PianoRow.js
+++ src/components/PianoRow/PianoRow.js
@@ -2,11 +2,30 @@ import React from 'react';
 import './PianoRow.css'
 
 const PianoRow = ({ name, note, steps, currentStepIndex, setSteps }) => {
-  
+
+  const toggleNote = (note, i) => {
+    let newSteps = [...steps];
+
+    if( !steps[i] ) {
+      newSteps[i] = [note]
+      setSteps(newSteps);
+      return
+    } else if(steps[i].includes(note)) {
+      newSteps[i] = newSteps[i].filter((nt) => nt !== note);
+      if(newSteps[i].length === 0) newSteps[i] = null;
+      setSteps(newSteps);
+      return
+    } else {
+      newSteps[i].push(note);
+      setSteps(newSteps);
+      return
+    }
+  }
+
   let row = (
     <div className='row'>
     {name ? <p>{name}</p> : <p> </p>}
-    {name ? steps.map((step, index) => steps[index] && steps[index].includes(note) ? <button onClick={() => toggleNote(note, index)} key={index}>{index}</button> : <button onClick={} >Pog</button>) : steps.map((step, index) => <p key={index}>{index + 1}</p>)}
+    {name ? steps.map((step, index) => steps[index] && steps[index].includes(note) ? <button onClick={() => toggleNote(note, index)} key={index} className='active piano-button'></button> : <button onClick={() => toggleNote(note, index)} key={index} className='piano-button'></button>) : steps.map((step, index) =><p key={index}>{index + 1}</p>)}
     </div>
   )
 
