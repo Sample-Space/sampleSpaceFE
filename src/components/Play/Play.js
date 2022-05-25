271b8739d0802f2d8e56549edd4146d73b39bf32
Eric Matlock <ermatlock@gmail.com>
2022-05-25 14:07:05 -0400

    Add error handling

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -7,12 +7,15 @@ import DrumPad from "../DrumPad/DrumPad";
 import InfoBox from "../InfoBox/InfoBox";
 import { Link } from "react-router-dom";
 import { Loader } from "../Loader/Loader";
+import { Error } from "../Error/Error";
 
 const Play = () => {
   const [kitNames, setKitNames] = useState([]);
   const [kit, setKit] = useState(null);
   const [currentSample, setCurrentSample] = useState(null);
   const [selectedKit, setSelectedKit] = useState("Andromeda%20Strain");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(true);
   const isFirstLoad = useRef(true);
 
   const kickRef = useRef(null);
@@ -69,17 +72,48 @@ const Play = () => {
   };
 
   useEffect(() => {
-    fetchKit("Andromeda%20Strain").then((data) => setKit(data));
+    fetchKit("Andromeda%20Strain")
+      .then((data) => {
+        setKit(data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setErrorMessage("Sorry, We were unable to get that. Please try again!");
+      });
   }, []);
 
   useEffect(() => {
-    if (isFirstLoad.current) {
-      isFirstLoad.current = false;
-    } else {
+    // if (isFirstLoad.current) {
+    //   isFirstLoad.current = false;
+    // } else {
+      setLoading(true)
       clearSamples();
-      fetchKit(selectedKit).then((data) => setKit(data));
-    }
+      fetchKit(selectedKit)
+        .then((data) => {
+          setKit(data);
+          setTimeout(() => {
+            setLoading(false);
+          }, 1500);
+        })
+        .catch((e) => {
+          setLoading(false);
+          setErrorMessage(
+            "Sorry, We were unable to get that. Please try again!"
+          );
+        });
+    // }
   }, [selectedKit]);
+
+  // useEffect(() => {
+  //   if (loading) {
+  //     setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
+  //   }
+  // }, [loading]);
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyboard);
@@ -88,21 +122,21 @@ const Play = () => {
     };
   }, [handleKeyboard]);
 
-  const getKitNames = () => {
-    fetchKitNames().then((data) => {
-      setKitNames(data);
-    });
-  };
+  // const getKitNames = () => {
+  //   fetchKitNames().then((data) => {
+  //     setKitNames(data);
+  //   });
+  // };
 
-  const getKit = (kitName) => {
-    fetchKit(kitName).then((kitData) => {
-      setKit(kitData);
-    });
-  };
+  // const getKit = (kitName) => {
+  //   fetchKit(kitName).then((kitData) => {
+  //     setKit(kitData);
+  //   });
+  // };
 
-  const changeKit = (selectedKit) => {
-    fetchKit(selectedKit).then((data) => setKit(data.kit));
-  };
+  // const changeKit = (selectedKit) => {
+  //   fetchKit(selectedKit).then((data) => setKit(data.kit));
+  // };
 
   return (
     <div className="main-view">
@@ -123,9 +157,12 @@ const Play = () => {
           <option value="Apollo%2011">Apollo 11</option>
         </select>
       </header>
-      {/* <Loader /> */}
-      {!kit ? <Loader /> : (
-        <main className="main-container">
+      {loading && !errorMessage ? (
+        <Loader />
+      ) : errorMessage ? (
+        <Error error={errorMessage} />
+      ) : (
+        kit && <main className="main-container">
           <DrumPad
             kit={kit.kit}
             setCurrentSample={setCurrentSample}
