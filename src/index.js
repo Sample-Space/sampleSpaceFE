e99f8589ab5e4265522516b96aa1fc1afb572f73
Eric Matlock <ermatlock@gmail.com>
2022-05-24 20:24:39 -0400

    Add stylyng to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import Play from './components/Play/Play'
+import "./index.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
