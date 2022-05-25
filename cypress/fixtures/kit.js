9baa8ccadaee2b31cfb797df830b2180a356661c
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-25 16:34:28 -0600

    Create kit fixture

--- cypress/fixtures/kit.js
+++ cypress/fixtures/kit.js
@@ -8,64 +8,64 @@
             "kick": {
                 "name": "K_Star 808 Kepler KIC",
                 "source_name": "kepler",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/kick_kepler_star_808.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=663e450474331530707dac70c924ec0e95b227d0cbd4d8c56ae71536095f3bdd",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/kepler.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=a4c251bb649ea165a06ea037e9f2fa8ff497d6da19c16a0f3167aa4e5df41356",
+                "sound_url": "../../public/assets/samples/kick_earth_magnetic_drum.mp3",
+                "thumbnail_url": "https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
                 "description": "Kepler: Star KIC7671081B Light Curve Waves to Sound",
                 "info_url": "https://www.nasa.gov/mission_pages/kepler/overview/index.html"
             },
             "snare": {
                 "name": "SN_Plasmasphere Polar",
                 "source_name": "nasa_polar",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/snare_plasmaspheric_hiss.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=cca8372b78711baa5e7d8d1528bfb8a7d8686eed3cabcf3357a338b1bf9b74c5",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/nasa_polar.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=315f10c1291e4d90cd001d653d6453e22e8fc37e8aab70cfe96dcf50f15ed700",
+                "sound_url": "../../public/assets/samples/snare_juno_entering_jupiter_magnetosphere.mp3",
+                "thumbnail_url": "https://cdn-icons-png.flaticon.com/512/5539/5539593.png",
                 "description": "Plasmaspheric hiss waves as heard by NASA’s Polar mission as it passed around Earth.",
                 "info_url": "https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/2021JA029644"
             },
             "hh_closed": {
                 "name": "HH1_Radar Echoes Titan Huygens",
                 "source_name": "titan",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/hat_radar_echoes_titan_surface.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=e258c62673ce1cb9d92edc6b551013d0b8d69e9de1296ad8206c9db81f3f5038",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/titan.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=284af48956d125876626e82c0061e9e967388b7fab693627303de85d8d838b1a",
+                "sound_url": "../../public/assets/samples/hat_closed_cassini_saturn_radio_emmisions.mp3",
+                "thumbnail_url": "https://previews.123rf.com/images/imagevectors/imagevectors1603/imagevectors160300759/53026465-flat-black-jupiter-icon-web-icon-in-circle-on-white-background.jpg",
                 "description": "This recording was produced by converting into audible sounds some of the radar echoes received by Huygens during the last few kilometers of its descent onto Saturn's moon Titan. As the probe approaches the ground, both the pitch and intensity increase. Scientists will use intensity of the echoes to speculate about the nature of the surface.",
                 "info_url": "https://solarsystem.nasa.gov/missions/huygens/in-depth/"
             },
             "hh_open": {
                 "name": "HH2_Saturn Sounds Cassini",
                 "source_name": "cassini_saturn_radio_emmisions",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/hat_open_cassini_saturn_radio_emmisions_1.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=28410d9bc01833cb02d97313b0c3b42fc4eea67b6b72ab2fb1a35537f0377131",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/saturn_radio_emmissions.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=f3348ab0aeddb975ec00ab2cb726ed38960870443dd8a9303f5672c353ecb73c",
+                "sound_url": "../../public/assets/samples/hat_open_cassini_saturn_radio_emmisions_1.mp3",
+                "thumbnail_url": "https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
                 "description": "New research from the up-close Grand Finale orbits of NASA’s Cassini mission shows a surprisingly powerful interaction of plasma waves moving from Saturn to its moon Enceladus. Researchers converted the recording of plasma waves into a “whooshing” audio file that we can hear -- in the same way a radio translates electromagnetic waves into music. Much like air or water, plasma (the fourth state of matter) generates waves to carry energy. The recording was captured by the Radio Plasma Wave Science (RPWS) instrument Sept. 2, 2017, two weeks before Cassini was deliberately plunged into the atmosphere of Saturn.",
                 "info_url": "https://www.nasa.gov/mission_pages/cassini/main/index.html"
             },
             "melody": {
                 "name": "M_Plasma 9 Voyager 1",
                 "source_name": "voyager_1",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/melody_voyage_plasma_9.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=0077e91508ae56bb17779ae6a1793fd90e63e4d456db78abb7b8a1e4bbf357ed",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/voyager_1.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=af4e9fa1b0867227b9f877b708aca2a60e341a17acd6a783d2fcfddf98ea833f",
+                "sound_url": "../../public/assets/samples/melody_ultra_cold_helium.mp3",
+                "thumbnail_url": "https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
                 "description": "Voyager 1's plasma wave instrument detected the vibrations of dense interstellar plasma, or ionized gas, from October to November 2012 and April to May 2013. This sound reproduces the amplitude and frequency of the plasma waves as 'heard' by Voyager 1.",
                 "info_url": "https://www.youtube.com/watch?v=rutBta_MdmU"
             },
             "texture": {
                 "name": "OS_Whistler Waves EMFISIS",
                 "source_name": "earth",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/one_shot_whistler_waves.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=5c955c8fb0fff7631e3c619f6a618c8dd6d49951b8496e8e0439dede3e03ad8f",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/EMFISIS_earth.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=749428106d09c0436230fb55ccafc6b4daaa8ac5e1070dbee486e04b9840cca7",
+                "sound_url": "../../public/assets/samples/groove_cassini_shields_up.mp3",
+                "thumbnail_url": "https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
                 "description": "Whistler waves as heard by the EMFISIS instrument aboard NASA’s Van Allen Probes as it passed around Earth.",
                 "info_url": "https://vlfstanford.ku.edu.tr/research_topic_inlin/introduction-whistler-waves-magnetosphere/"
             },
             "one_shot_1": {
                 "name": "OS_Deep Space Hubble",
                 "source_name": "hubble_deep_space",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/one_shot_sonification_of_hubble_deep_space.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=48f479d300b7fb910520d42d0a191c7765e8c02f4535fd8ff5aa075e5aa9c5b1",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/sonification_of_hubble_deep_space.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=e7142e7064a82cfb1686491878789347440ae3933a55b208a1854cfc44a85c8f",
+                "sound_url": "../../public/assets/samples/one_shot_psp_langmuir_waves.mp3",
+                "thumbnail_url": "https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
                 "description": "Space becomes 'sonified' in this visualization of a cluster of galaxies imaged by NASA's Hubble Space Telescope. Time flows left to right, and the frequency of sound changes from bottom to top, ranging from 30 to 1,000 hertz. Objects near the bottom of the image produce lower notes, while those near the top produce higher ones. Most of the visible specks are galaxies housing countless stars. A few individual stars shine brightly in the foreground. Stars and compact galaxies create short, clear tones, while sprawling spiral galaxies emit longer notes that change pitch. The higher density of galaxies near the center of the image — the heart of this galaxy cluster, known as RXC J0142.9+4438 — results in a swell of mid-range tones halfway through the video. Hubble's Advanced Camera for Surveys and Wide Field Camera 3 acquired this image on Aug. 13, 2018.",
                 "info_url": "https://hubblesite.org/contents/articles/hubble-deep-fields"
             },
             "one_shot_2": {
                 "name": "G_Passing Tempel 1 Stardust",
                 "source_name": "tempel_1",
-                "sound_url": "https://sampe-space.s3.amazonaws.com/groove_stardust_passing_comet_temple_1.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=4c454bd12d2006f1dac4e49fd1d80e4c45cc01fce5b8ff4d1d8d322e0afbb4f1",
-                "thumbnail_url": "https://sampe-space.s3.amazonaws.com/tempel_1.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUJ552RO7A2PY6M6C%2F20220525%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220525T165242Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=7153c70183d5bc054ed8337390389f0682a53b5b3d5157852b736e05e470daec",
+                "sound_url": "../../public/assets/samples/one_shot_psp_whistler_mode_waves.mp3",
+                "thumbnail_url": "https://images.theconversation.com/files/394/original/See_Explanation._Clicking_on_the_picture_will_download_the_highest_resolution_version_available.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
                 "description": "Stardust: Passing Comet Tempel 1",
                 "info_url": "https://solarsystem.nasa.gov/resources/331/stardust-swoops-by-tempel-1/"
             }
