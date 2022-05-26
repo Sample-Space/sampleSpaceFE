f46d3d3c16df64f6316daaa356eacfd28859a63b
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-05-26 10:38:27 -0600

    Error component now dynamic to handle noMatch

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./Error.css";
 import { Link } from "react-router-dom";
 
-export const Error = ({ errorMessage }) => {
+export const Error = ({ errorMessage, noMatch }) => {
+  const message = noMatch ? (
+    <h1>Sorry, this part of space is empty!</h1>
+  ) : (
+    <h1>Sorry, we're unable to get that. Please try again!</h1>
+  )
   return (
     <div className="error">
-      <h1>Sorry, we're unable to get that. Please try again!</h1>
+      {message}
       <Link to="/">
         <button className="go-back-button">Home</button>
       </Link>
